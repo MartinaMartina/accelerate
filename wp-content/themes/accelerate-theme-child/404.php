5d4e77cb40b420cfd04ecf4dfc280173245180d3
MartinaMartina <martinamartina@gmail.com>
2017-04-01 00:49:25 -0400

    twitter module on homepage, social share buttons, fixed top nav

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for 404 pages
+ *
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div class="fourohfour">
+		Blame it on Donna.
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -52,18 +52,35 @@ get_header(); ?>
 <section class="recent-posts">
 	<div class="site-content">
     	<div class="blog-post">
-          <h4>From the Blog</h4>
+          <h5>From the Blog</h5>
 		 	 <?php query_posts('posts_per_page=1'); ?>
 			   <?php while ( have_posts() ) : the_post(); ?>
-				   <h2><?php the_title(); ?></h2>
+				   <h3><?php the_title(); ?></h3>
 				   <?php the_excerpt(); ?>
 				   <a class="read-more-link" href="<?php the_permalink(); ?>">Read More <span>&rsaquo;</span></a>
 				
 			   <?php endwhile; ?> 
 			 <?php wp_reset_query(); ?>
     	</div>
+		
+		<div class="tweeting">
+			
+			<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+				<div id="secondary" class="widget-area" role="complementary">
+					<h5>Recent Tweets</h5>
+					<?php dynamic_sidebar( 'sidebar-2' ); ?>
+					<a class="read-more-link" href="http://www.twitter.com/martinamartina">Follow us <span>&rsaquo;</span></a>
+				</div>
+			<?php endif; ?>
+			
+		</div>
+		
 	</div>
 </section>
+
+<div class="spacing">&nbsp;</div>
+
+
 
 
 <!-- .home-page -->
